b35ed0d1d1997f51dbec7ac581824d830b5d0e1b
jaja42155 <jalalbonnemeur@gmail.com>
2026-09-25 13:16:34 +0200

    Variable

---
+++ cssVariable/sscript.js
@@ -0,0 +1,9 @@
+// Définir la variable permettant de récupérer l'input de type color
+
+
+/* A cette variable, appliquer la méthode (addEventListener) écoutant l'événement de type change et jouant la fonction permettant d'ajouter une propriété de style (SetProperty) au document :
+- le nom de la propriété est la variable CSS --main-color 
+- La valeur est définie par la valeur de l'input
+*/
+
+const get
